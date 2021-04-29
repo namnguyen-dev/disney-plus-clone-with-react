054dec53947793fbcebb4c899e63eb0e72c9979e
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2021-04-29 13:22:27 +0700

    add Logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useEffect } from 'react';
 import { auth, provider } from '../firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
@@ -6,6 +7,7 @@ import {
   selectUserName,
   selectUserPhoto,
   setUserLoginDetails,
+  setSignOutState,
 } from '../features/user/userSlice';
 
 const Header = props => {
@@ -14,15 +16,40 @@ const Header = props => {
   const userName = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
 
-  const handleAuth = () => {
-    auth
-      .signInWithPopup(provider)
-      .then(result => {
-        setUser(result.user);
-      })
-      .catch(error => {
-        alert(error.message);
+  useEffect(() => {
+    if (!userName) {
+      auth.onAuthStateChanged(async user => {
+        console.log(user);
+        if (user) {
+          setUser(user);
+          history.push('/home');
+        }
       });
+    }
+  }, [userName]);
+
+  const handleAuth = () => {
+    if (!userName) {
+      auth
+        .signInWithPopup(provider)
+        .then(result => {
+          setUser(result.user);
+          // history.push('/home');
+        })
+        .catch(error => {
+          alert(error.message);
+        });
+    } else {
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setSignOutState());
+          history.push('/');
+        })
+        .catch(error => {
+          alert(error.message);
+        });
+    }
   };
 
   const setUser = user => {
@@ -71,11 +98,14 @@ const Header = props => {
               <span>SERIES</span>
             </a>
           </NavMenu>
-          <UserImg src={userPhoto} alt={userName} />
+          <Signout>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+              <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
+          </Signout>
         </>
       )}
-
-    
     </Nav>
   );
 };
@@ -189,6 +219,44 @@ const Login = styled.a`
 
 const UserImg = styled.img`
   height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: 0px 0px 18px 0px rgb(0 0 0/50%);
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const Signout = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    height: 100%;
+    width: 100%;
+  }
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 
 export default Header;
